071c7a49b2b317c0e016c32665b48e270c8ee239
Brazz26 <147454142+Brazz26@users.noreply.github.com>
2023-11-21 23:16:50 -0500

    created blocks for the schedule from 1200 to 1800 to represent future events

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,15 @@
-console.log('Hello')
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var saveBtnEl = document.querySelector('#fa-save');
 
 const currentDate = dayjs();
 const formattedDate = currentDate.format('YYYY-MM-DD');
 console.log(formattedDate);
-$(function () {
-    // var currentDay = dayjs();
-    // $('currentDay') = dayjs().text();
+console.log
+$(function dataSubmit(event) {
+
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
